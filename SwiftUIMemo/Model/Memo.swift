f7fbb57e08e8e32c67c9fbd807204f991ef5e96f
youngsik_won <youngsik3200@gmail.com>
2020-08-23 19:00:16 +0900

    first commit

---
+++ SwiftUIMemo/Model/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by Youngsik on 2020/08/23.
+//  Copyright © 2020 Youngsik. All rights reserved.
+//
+
+import SwiftUI
+
+struct Memo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Memo_Previews: PreviewProvider {
+    static var previews: some View {
+        Memo()
+    }
+}
---
+++ SwiftUIMemo/Model/MemoStore.swift
@@ -0,0 +1,9 @@
+//
+//  MemoStore.swift
+//  SwiftUIMemo
+//
+//  Created by Youngsik on 2020/08/23.
+//  Copyright © 2020 Youngsik. All rights reserved.
+//
+
+import Foundation
